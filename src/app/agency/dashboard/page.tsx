b927660e7e1664b8c7f7aead3cd20a467019c69c
VanHocVH <vovanhoc12@gmail.com>
2023-09-25 14:41:01 +0700

    rebase main

--- src/app/agency/dashboard/page.tsx
+++ src/app/agency/dashboard/page.tsx
@@ -1,12 +1,14 @@
+
+import DashBoard from '@/components/pageComponent/dashboard';
 import * as React from 'react';
 
-export interface  AgencyDashBoardProps {
+export interface  IDashboardPageProps {
 }
 
-export default function AgencyDashBoard (props:  AgencyDashBoardProps) {
-  return (
-    <div>
-      dashboard
-    </div>
-  );
-}
+export default function DashboardPage(props: IDashboardPageProps) {
+    return (
+        <div>
+            <DashBoard />
+        </div>
+    );
+  }
